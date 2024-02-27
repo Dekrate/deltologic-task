0ef7d30f253840e58f68195c8ab0bcec23cb706d
Dekrate <mikdiakowski@interia.pl>
2024-02-27 15:08:55 +0100

    feat: tests

--- source/app.test.ts
+++ source/app.test.ts
@@ -1,19 +1,18 @@
-import request from "supertest";
-import app from "./api.controller"
+import supertest from 'supertest';
+import app from './api.controller';
 
-describe("API Controller", () => {
-
-    it("should return 2", async () => {
-        const response = await request(app)
+describe('API Controller', () => {
+    it('should return 2', async () => {
+        const response = await supertest(app)
             .post('/api')
-            .send({ height: [3, 2, 4, 1, 2] });
+            .send({height: [3, 2, 4, 1, 2]});
         expect(response.body.result).toEqual(2);
     });
 
-    it("should return 8", async () => {
-        const response = await request(app)
+    it('should return 8', async () => {
+        const response = await supertest(app)
             .post('/api')
-            .send({ height: [4, 1, 1, 0, 2, 3] });
+            .send({height: [4, 1, 1, 0, 2, 3]});
         expect(response.body.result).toEqual(8);
-    });
+    })
 });
